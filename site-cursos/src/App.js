e6abdcac02ffa548b4d677bda0899b87c91101bf
drohiro <rodrigorfsfr3@gmail.com>
2019-10-19 09:15:04 -0300

    aula 02

--- site-cursos/src/App.js
+++ site-cursos/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 import '../node_modules/font-awesome/css/font-awesome.min.css';
+import '../node_modules/jquery/dist/jquery.min';
+import '../node_modules/popper.js/dist/umd/popper.min';
+import '../node_modules/bootstrap/dist/js/bootstrap.min';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
 import Cursos from './pages/cursos'
 import Contato from './pages/contato'
+import Menu from './components/menu'
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Menu></Menu>
       <Cursos></Cursos>
       <Contato></Contato>
     </div>
