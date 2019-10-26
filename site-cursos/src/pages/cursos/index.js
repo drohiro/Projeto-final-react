535d277a20d4865fc6f533490045795ecd9d4b2a
drohiro <rodrigorfsfr3@gmail.com>
2019-10-26 12:37:49 -0300

    Update index.js

--- site-cursos/src/pages/cursos/index.js
+++ site-cursos/src/pages/cursos/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Cabecalho from '../../components/menu/Cabecalho'
 import Cadastro from './cadastro';
 
-
 export default class Curso extends Component {
     render() {
         return (
